495ead83e113f5bee835074367d03789d580bc3c
carolinevannebo <carolinevannebo@icloud.com>
2024-10-18 20:09:39 +0200

    implemented threads, fixed reading of input files, need to move `runInput` logic from main to client and figure out how to solve broadcasting

--- src/main/java/com/in5020/group4/Transaction.java
+++ src/main/java/com/in5020/group4/Transaction.java
@@ -1,8 +1,14 @@
 package com.in5020.group4;
 
 public class Transaction {
-    public String uniqueId;
+    public int uniqueId;
     public String command;
+
+    public Transaction() {}
+    public Transaction(int uniqueId, String command) {
+        this.uniqueId = uniqueId;
+        this.command = command;
+    }
 
     public void setCommand(String command) {
         this.command = command;
@@ -12,11 +18,11 @@ public class Transaction {
         return command;
     }
 
-    public void setUniqueId(String uniqueId) {
+    public void setUniqueId(int uniqueId) {
         this.uniqueId = uniqueId;
     }
 
-    public String getUniqueId() {
+    public int getUniqueId() {
         return uniqueId;
     }
 }
